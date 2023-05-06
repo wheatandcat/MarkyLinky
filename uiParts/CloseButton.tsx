9c8b86e994ba275577830efa92762d2f1973c37e
Yohei Iino <wheatandcat@gmail.com>
2023-05-06 17:15:16 +0900

    update (#7)

--- uiParts/CloseButton.tsx
+++ uiParts/CloseButton.tsx
@@ -6,11 +6,11 @@ function CloseButton(props: Props) {
   return (
     <button
       type="button"
-      className="bg-white rounded-md inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
+      className="rounded-md inline-flex items-center justify-center hover:bg-gray-200"
       onClick={() => props.onRemove()}>
       <span className="sr-only">Close menu</span>
       <svg
-        className="h-4 w-4"
+        className="h-4 w-4 text-red-400"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"

030239a8f46477037b1cc8c7b175a677e8bc0dfd
wheatandcat <wheatandcat@gmail.com>
2023-07-01 22:32:51 +0900

    add: options

--- core/supabase.ts
+++ core/supabase.ts
@@ -5,14 +5,3 @@ export const supabase = createClient<Database>(
   process.env.PLASMO_PUBLIC_SUPABASE_URL,
   process.env.PLASMO_PUBLIC_SUPABASE_KEY,
 );
-
-export const supabaseServer = createClient<Database>(
-  process.env.PLASMO_PUBLIC_SUPABASE_URL,
-  process.env.PLASMO_PUBLIC_SUPABASE_KEY,
-  {
-    auth: {
-      autoRefreshToken: false,
-      persistSession: false,
-    },
-  },
-);
